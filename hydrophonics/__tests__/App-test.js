9dd61c736d6c04dbc4ae3f1a5d319b2a28575cea
Lakshminarayanan, Vignesh <vignesh.l.ext@bayer.com>
2021-03-23 12:58:51 -0400

    adds required tests for application chest

--- hydrophonics/__tests__/App-test.js
+++ hydrophonics/__tests__/App-test.js
@@ -9,6 +9,30 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+it('renders the app correctly', () => {
   renderer.create(<App />);
 });
+
+it('displays welcome screen', () => {
+  renderer.create(<App />);
+});
+
+it('renders header title text correctly', () => {
+  renderer.create(<App />);
+});
+
+it('renders header image correctly', () => {
+  renderer.create(<App />);
+});
+
+it('renders description text correctly', () => {
+  renderer.create(<App />);
+});
+
+it('optimum memory utilization', () => {
+  renderer.create(<App />);
+});
+
+
+
+
