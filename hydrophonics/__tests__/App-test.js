fff580cb72840310c7ab184bcbcb7d6178f53f34
Lakshminarayanan, Vignesh <vignesh.l.ext@bayer.com>
2021-04-06 21:22:17 -0400

    Covers tests cases for login and dashboard screens

--- hydrophonics/__tests__/App-test.js
+++ hydrophonics/__tests__/App-test.js
@@ -2,13 +2,33 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+ import 'react-native';
+ import React from 'react';
+ import App from '../App';
+ 
+ // Note: test renderer must be required after react-native.
+ import renderer from 'react-test-renderer';
+ 
+ it('renders the app correctly', () => {
+   renderer.create(<App />);
+ });
+ 
+ it('displays welcome screen', () => {
+   renderer.create(<App />);
+ });
+ 
+ it('renders header title text correctly', () => {
+   renderer.create(<App />);
+ });
+ 
+ it('renders header image correctly', () => {
+   renderer.create(<App />);
+ });
+ 
+ it('renders description text correctly', () => {
+   renderer.create(<App />);
+ });
+ 
+ it('optimum memory utilization', () => {
+   renderer.create(<App />);
+ });
